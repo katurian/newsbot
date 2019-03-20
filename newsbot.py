b24eb2770f66960975b8e70372f28c2d91b2bac0
Kate Kulinski <35544223+katurian@users.noreply.github.com>
2019-03-20 16:02:58 -0400

    Update newsbot.py

--- newsbot.py
+++ newsbot.py
@@ -7,7 +7,7 @@ import platform
 import feedparser
 
 
-client = Bot(description="Newsbot by Kat", command_prefix="news-", pm_help = False)
+client = Bot(description="Newsbot by Kat", command_prefix="!", pm_help = False)
 
 @client.event
 async def on_ready():
@@ -18,193 +18,184 @@ async def on_ready():
 	print('Made by Kate Kulinski')
 	return await client.change_presence(game=discord.Game(name='PLAYING STATUS HERE'))
 
+@client.command()
+async def politics(*args):
+	d = feedparser.parse('https://www.economist.com/the-world-this-week/rss.xml')
+	link = d['items'][1]['link']
+	await client.say(link)
+	await asyncio.sleep(3)
 
 @client.command()
-async def world(*args):
-	d = feedparser.parse('https://www.economist.com/rss/the_world_this_week_rss.xml')
-	link = d['entries'][2]['id']
+async def business(*args):
+	d = feedparser.parse('https://www.economist.com/the-world-this-week/rss.xml')
+	link = d['items'][0]['link']
 	await client.say(link)
 	await asyncio.sleep(3)
 
 @client.command()
 async def china(*args):
-	d = feedparser.parse('https://www.economist.com/sections/china/rss.xml')
-	link1 = d['entries'][0]['link']
+	d = feedparser.parse('https://www.economist.com/china/rss.xml')
+	link1 = d['items'][0]['link']
 	await client.say(link1)
 	await asyncio.sleep(3)
-	link2 = d['entries'][1]['link']
+	link2 = d['items'][1]['link']
 	await client.say(link2)
 	await asyncio.sleep(3)
-	link3 = d['entries'][2]['link']
+	link3 = d['items'][2]['link']
 	await client.say(link3)
 	await asyncio.sleep(3)
-	link4 = d['entries'][3]['link']
+	link4 = d['items'][3]['link']
 	await client.say(link4)
 
 @client.command()
 async def us(*args):
-	d = feedparser.parse('https://www.economist.com/sections/united-states/rss.xml')
-	link1 = d['entries'][0]['link']
+	d = feedparser.parse('https://www.economist.com/united-states/rss.xml')
+	link1 = d['items'][0]['link']
 	await client.say(link1)
 	await asyncio.sleep(3)
-	link2 = d['entries'][1]['link']
+	link2 = d['items'][1]['link']
 	await client.say(link2)
 	await asyncio.sleep(3)
-	link3 = d['entries'][2]['link']
+	link3 = d['items'][2]['link']
 	await client.say(link3)
 	await asyncio.sleep(3)
-	link4 = d['entries'][3]['link']
+	link4 = d['items'][3]['link']
 	await client.say(link4)
 
 @client.command()
 async def americas(*args):
-	d = feedparser.parse('https://www.economist.com/sections/americas/rss.xml')
-	link1 = d['entries'][0]['link']
+	d = feedparser.parse('https://www.economist.com/the-americas/rss.xml')
+	link1 = d['items'][0]['link']
 	await client.say(link1)
 	await asyncio.sleep(3)
-	link2 = d['entries'][1]['link']
+	link2 = d['items'][1]['link']
 	await client.say(link2)
 	await asyncio.sleep(3)
-	link3 = d['entries'][2]['link']
+	link3 = d['items'][2]['link']
 	await client.say(link3)
 	await asyncio.sleep(3)
-	link4 = d['entries'][3]['link']
+	link4 = d['items'][3]['link']
 	await client.say(link4)
 
 @client.command()
 async def tech(*args):
-	d = feedparser.parse('https://www.economist.com/sections/science-technology/rss.xml')
-	link1 = d['entries'][0]['link']
+	d = feedparser.parse('https://www.economist.com/science-and-technology/rss.xml')
+	link1 = d['items'][0]['link']
 	await client.say(link1)
 	await asyncio.sleep(3)
-	link2 = d['entries'][1]['link']
+	link2 = d['items'][1]['link']
 	await client.say(link2)
 	await asyncio.sleep(3)
-	link3 = d['entries'][2]['link']
+	link3 = d['items'][2]['link']
 	await client.say(link3)
 	await asyncio.sleep(3)
-	link4 = d['entries'][3]['link']
+	link4 = d['items'][3]['link']
 	await client.say(link4)
 
 @client.command()
 async def more_tech(*args):
-	d = feedparser.parse('https://www.economist.com/sections/science-technology/rss.xml')
-	link5 = d['entries'][4]['link']
+	d = feedparser.parse('https://www.economist.com/science-and-technology/rss.xml')
+	link5 = d['items'][4]['link']
 	await client.say(link5)
 	await asyncio.sleep(3)
-	link6 = d['entries'][5]['link']
+	link6 = d['items'][5]['link']
 	await client.say(link6)
 	await asyncio.sleep(3)
-	link7 = d['entries'][6]['link']
+	link7 = d['items'][6]['link']
 	await client.say(link7)
 	await asyncio.sleep(3)
-	link8 = d['entries'][7]['link']
+	link8 = d['items'][7]['link']
 	await client.say(link8)
 
 @client.command()
 async def econ(*args):
-	d = feedparser.parse('https://www.economist.com/sections/economics/rss.xml')
-	link1 = d['entries'][0]['link']
+	d = feedparser.parse('https://www.economist.com/finance-and-economics/rss.xml')
+	link1 = d['items'][0]['link']
 	await client.say(link1)
 	await asyncio.sleep(3)
-	link2 = d['entries'][1]['link']
+	link2 = d['items'][1]['link']
 	await client.say(link2)
 	await asyncio.sleep(3)
-	link3 = d['entries'][2]['link']
+	link3 = d['items'][2]['link']
 	await client.say(link3)
 	await asyncio.sleep(3)
-	link4 = d['entries'][3]['link']
+	link4 = d['items'][3]['link']
 	await client.say(link4)
 
 
 @client.command()
 async def britain(*args):
-	d = feedparser.parse('https://www.economist.com/sections/britain/rss.xml')
-	link1 = d['entries'][0]['link']
+	d = feedparser.parse('https://www.economist.com/britain/rss.xml')
+	link1 = d['items'][0]['link']
 	await client.say(link1)
 	await asyncio.sleep(3)
-	link2 = d['entries'][1]['link']
+	link2 = d['items'][1]['link']
 	await client.say(link2)
 	await asyncio.sleep(3)
-	link3 = d['entries'][2]['link']
+	link3 = d['items'][2]['link']
 	await client.say(link3)
 	await asyncio.sleep(3)
-	link4 = d['entries'][3]['link']
+	link4 = d['items'][3]['link']
 	await client.say(link4)
-
-@client.command()
-async def culture(*args):
-	d = feedparser.parse('https://www.economist.com/sections/culture/rss.xml')
-	link1 = d['entries'][0]['link']
-	await client.say(link1)
-	await asyncio.sleep(3)
-	link2 = d['entries'][1]['link']
-	await client.say(link2)
-	await asyncio.sleep(3)
-	link3 = d['entries'][2]['link']
-	await client.say(link3)
-	await asyncio.sleep(3)
-	link4 = d['entries'][3]['link']
-	await client.say(link4)
-
-@client.command()
-async def more_culture(*args):
-	d = feedparser.parse('https://www.economist.com/sections/culture/rss.xml')
-	link5 = d['entries'][4]['link']
-	await client.say(link5)
-	await asyncio.sleep(3)
-	link6 = d['entries'][5]['link']
-	await client.say(link6)
-	await asyncio.sleep(3)
-	link7 = d['entries'][6]['link']
-	await client.say(link7)
-	await asyncio.sleep(3)
-	link8 = d['entries'][7]['link']
-	await client.say(link8)
 
 @client.command()
 async def africa(*args):
-	d = feedparser.parse('https://www.economist.com/sections/middle-east-africa/rss.xml')
-	link1 = d['entries'][0]['link']
+	d = feedparser.parse('https://www.economist.com/middle-east-and-africa/rss.xml')
+	link1 = d['items'][0]['link']
 	await client.say(link1)
 	await asyncio.sleep(3)
-	link2 = d['entries'][1]['link']
+	link2 = d['items'][1]['link']
 	await client.say(link2)
 	await asyncio.sleep(3)
-	link3 = d['entries'][2]['link']
+	link3 = d['items'][2]['link']
 	await client.say(link3)
 	await asyncio.sleep(3)
-	link4 = d['entries'][3]['link']
+	link4 = d['items'][3]['link']
+	await client.say(link4)
+
+@client.command()
+async def middle(*args):
+	d = feedparser.parse('https://www.economist.com/middle-east-and-africa/rss.xml')
+	link1 = d['items'][0]['link']
+	await client.say(link1)
+	await asyncio.sleep(3)
+	link2 = d['items'][1]['link']
+	await client.say(link2)
+	await asyncio.sleep(3)
+	link3 = d['items'][2]['link']
+	await client.say(link3)
+	await asyncio.sleep(3)
+	link4 = d['items'][3]['link']
 	await client.say(link4)
 
 @client.command()
 async def europe(*args):
-	d = feedparser.parse('https://www.economist.com/sections/europe/rss.xml')
-	link1 = d['entries'][0]['link']
+	d = feedparser.parse('https://www.economist.com/europe/rss.xml')
+	link1 = d['items'][0]['link']
 	await client.say(link1)
 	await asyncio.sleep(3)
-	link2 = d['entries'][1]['link']
+	link2 = d['items'][1]['link']
 	await client.say(link2)
 	await asyncio.sleep(3)
-	link3 = d['entries'][2]['link']
+	link3 = d['items'][2]['link']
 	await client.say(link3)
 	await asyncio.sleep(3)
-	link4 = d['entries'][3]['link']
+	link4 = d['items'][3]['link']
 	await client.say(link4)
 
 @client.command()
 async def asia(*args):
-	d = feedparser.parse('https://www.economist.com/sections/asia/rss.xml')
-	link1 = d['entries'][0]['link']
+	d = feedparser.parse('https://www.economist.com/asia/rss.xml')
+	link1 = d['items'][0]['link']
 	await client.say(link1)
 	await asyncio.sleep(3)
-	link2 = d['entries'][1]['link']
+	link2 = d['items'][1]['link']
 	await client.say(link2)
 	await asyncio.sleep(3)
-	link3 = d['entries'][2]['link']
+	link3 = d['items'][2]['link']
 	await client.say(link3)
 	await asyncio.sleep(3)
-	link4 = d['entries'][3]['link']
+	link4 = d['items'][3]['link']
 	await client.say(link4)
 
-client.run('BOT TOKEN')
+client.run('NTU4MDA2OTUzMDQ2MTc5ODYw.D3QkBA.eCxULGtdGYQDBRA7Ixnh_Qk31RI')
